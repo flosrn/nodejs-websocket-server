025df0fbe8a4635db6b52c92443accdc49a6376c
Flosrn <flosrn.dev@gmail.com>
2023-07-04 18:52:16 +0200

    add midjourney lib

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ config();
 
 import { WebSocketServer } from "ws";
 import { keepAlive } from "./utils/keepalive";
-// import { Midjourney } from "midjourney";
+import { Midjourney } from "midjourney";
 
 const wss = new WebSocketServer({ port: Number(process.env.PORT) });
 
@@ -12,25 +12,25 @@ wss.on("connection", (ws) => {
     const prompt = event.toString();
     console.log("prompt:", prompt);
 
-    // const client = new Midjourney({
-    //   ServerId: <string>process.env.SERVER_ID,
-    //   ChannelId: <string>process.env.CHANNEL_ID,
-    //   SalaiToken: <string>process.env.DISCORD_SALAI_TOKEN,
-    //   HuggingFaceToken: <string>process.env.HUGGINGFACE_TOKEN,
-    //   Debug: true,
-    //   Ws: true,
-    // });
-    // await client.init();
+    const client = new Midjourney({
+      ServerId: <string>process.env.SERVER_ID,
+      ChannelId: <string>process.env.CHANNEL_ID,
+      SalaiToken: <string>process.env.DISCORD_SALAI_TOKEN,
+      HuggingFaceToken: <string>process.env.HUGGINGFACE_TOKEN,
+      Debug: true,
+      Ws: true,
+    });
+    await client.init();
 
-    // const result = await client.Imagine(
-    //   prompt,
-    //   (uri: string, progress: string) => {
-    //     ws.send(JSON.stringify({ uri, progress }));
-    //   }
-    // );
+    const result = await client.Imagine(
+      prompt,
+      (uri: string, progress: string) => {
+        ws.send(JSON.stringify({ uri, progress }));
+      }
+    );
 
-    ws.send(JSON.stringify(prompt));
-    // client.Close();
+    ws.send(JSON.stringify(result));
+    client.Close();
   });
 });
 
