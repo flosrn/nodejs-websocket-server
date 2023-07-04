27af14549bd99d79af2c56764f9265fd8579996a
Flosrn <flosrn.dev@gmail.com>
2023-07-04 18:14:57 +0200

    add midjourney websocket

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,40 @@ import { config } from "dotenv";
 config();
 
 import { createServer } from "http";
+import { Server as WebsocketServer } from "ws";
+import { Midjourney } from "midjourney";
 
-createServer((_, res) => {
-  console.log("process.env.PORT : ", process.env.PORT);
-  res.write("Hello World!");
-  res.end();
-}).listen(process.env.PORT);
+const httpServer = createServer();
+
+httpServer.listen(process.env.PORT, () => {
+  console.log(`Server is listening on ${process.env.PORT}`);
+});
+
+const wsServer = new WebsocketServer({ server: httpServer });
+
+wsServer.on("connection", (ws) => {
+  ws.on("message", async (event) => {
+    const prompt = event.toString();
+    console.log("prompt:", prompt);
+
+    const client = new Midjourney({
+      ServerId: <string>process.env.SERVER_ID,
+      ChannelId: <string>process.env.CHANNEL_ID,
+      SalaiToken: <string>process.env.DISCORD_SALAI_TOKEN,
+      HuggingFaceToken: <string>process.env.HUGGINGFACE_TOKEN,
+      Debug: true,
+      Ws: true,
+    });
+    await client.init();
+
+    const result = await client.Imagine(
+      prompt,
+      (uri: string, progress: string) => {
+        ws.send(JSON.stringify({ uri, progress }));
+      }
+    );
+
+    ws.send(JSON.stringify(result));
+    client.Close();
+  });
+});
