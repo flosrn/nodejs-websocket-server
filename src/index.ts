d1f858c3f6ce06768b91e322ff8439019840f607
Flosrn <flosrn.dev@gmail.com>
2023-07-05 11:37:31 +0200

    Perform error handling in WebSocket message event
    When handling a message in the WebSocket connection, add error handling to catch any errors that may occur. This is important for providing better feedback to the client in case of an error.

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,41 @@
 import { WebSocketServer } from "ws";
 import { Midjourney } from "midjourney";
 import { config } from "dotenv";
+
 config();
 
 const wss = new WebSocketServer({ port: Number(process.env.PORT) });
 
 wss.on("connection", (ws) => {
   ws.on("message", async (event) => {
-    const prompt = event.toString();
-    console.log("prompt:", prompt);
+    try {
+      const prompt = event.toString();
+      console.log("prompt:", prompt);
 
-    const client = new Midjourney({
-      ServerId: <string>process.env.SERVER_ID,
-      ChannelId: <string>process.env.CHANNEL_ID,
-      SalaiToken: <string>process.env.DISCORD_SALAI_TOKEN,
-      HuggingFaceToken: <string>process.env.HUGGINGFACE_TOKEN,
-      Debug: true,
-      Ws: true,
-    });
-    await client.init();
-    const result = await client.Imagine(
-      prompt,
-      (uri: string, progress: string) => {
-        ws.send(JSON.stringify({ uri, progress }));
-      }
-    );
+      const client = new Midjourney({
+        ServerId: <string>process.env.SERVER_ID,
+        ChannelId: <string>process.env.CHANNEL_ID,
+        SalaiToken: <string>process.env.DISCORD_SALAI_TOKEN,
+        HuggingFaceToken: <string>process.env.HUGGINGFACE_TOKEN,
+        Debug: true,
+        Ws: true,
+      });
+      await client.init();
 
-    ws.send(JSON.stringify(result));
-    ws.send(JSON.stringify({ done: true }));
-    client.Close();
+      const result = await client.Imagine(
+        prompt,
+        (uri: string, progress: string) => {
+          ws.send(JSON.stringify({ uri, progress }));
+        }
+      );
+
+      ws.send(JSON.stringify(result));
+      ws.send(JSON.stringify({ done: true }));
+      client.Close();
+    } catch (error) {
+      console.error("An error occurred:", error);
+      ws.send(JSON.stringify({ error: error.message }));
+    }
   });
 });
 
